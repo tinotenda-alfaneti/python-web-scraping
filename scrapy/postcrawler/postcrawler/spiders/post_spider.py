376dff86357df426ba76026af22df37d0467e89a
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2023-06-11 01:42:02 +0000

    Update - Collect specific data

--- scrapy/postcrawler/postcrawler/spiders/post_spider.py
+++ scrapy/postcrawler/postcrawler/spiders/post_spider.py
@@ -8,7 +8,21 @@ class PostsSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        filename = 'eurusd-news.html'
+        filename = 'fx-news.html'
+        sections = response.css('div.awyw')
+        page_content = ''
+        for sect in sections:
+            sect_main = f"<h1>{sect.css('div.wa-head h5::text').get()}<h1>"
+            sect_news = sect.css('div.wa-body').css('div.clear h6 a')
+            sect_links = sect_news.xpath('@href').extract()
+            sect_headings = sect_news.xpath('text()').extract()
+            sect_times = sect.css('div.wa-body').css('div.clear time::text').getall()
+            page_content += sect_main
 
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+            for i in range(len(sect_headings)):
+                entry = f"<a href='{sect_links[i]}'>{sect_headings[i]} POSTED AT {sect_times[i]}</a><br>"
+                page_content += entry 
+
+
+        with open(filename, 'w') as f:
+            f.write(page_content)
